55b8a29f5c9a21ff71c8ead7454075314d23c264
Dheeruvaii <dhirajsapkota260@gmail.com>
2023-12-27 15:17:44 +0545

    recorrect callback functions

--- Likes/like_app/consumer.py
+++ Likes/like_app/consumer.py
@@ -3,11 +3,10 @@ import pika
 import django
 from sys import path 
 from os import environ
-import sys
-print(sys.path)
+  
 
 path.append('C:/Users/4/Desktop/RabbitMq-Practices/Likes')
-environ.setdefault('DJANGO_SETTINGS_MODULE','Likes.settings')
+environ.setdefault('DJANGO_SETTINGS_MODULE', 'Likes.settings')
 django.setup()
 
 from like_app.models import Quote
@@ -16,27 +15,28 @@ channel=connection.channel()
 channel.queue_declare(queue='likes')
 
 
-def callback(ch,method,properties,body):
-    print("recieved in likes...")
-    print(body)
-    data=json.loads(body)
-    print(data)
+def callback (ch, method, properties, body):
+        print("Received in likes...")
+        print("Body:", body)
+        print("Content Type:", properties.content_type)
+        data = json.loads(body)
+        print("Data:", data)
 
-    if properties.content_type=='quote_created':
-        quote=Quote.objects.create(id=data['id'],title=data['title'])
-        quote.save()
-        print('quote_created')
-    elif properties.content_type=='quote_updated':
-        quote=Quote.objects.get(id=data['id'])
-        quote.title=data['title']
-        quote.save()
-        print("quote_updated")
+        if properties.content_type=='quote_created':
+            quote=Quote.objects.create(id=data['id'],title=data['title'])
+            quote.save()
+            print('quote_created')
+        elif properties.content_type=='quote_updated':
+            quote=Quote.objects.get(id=data['id'])
+            quote.title=data['title']
+            quote.save()
+            print("quote_updated")
 
 
-    elif properties.content_type== 'quote_deleted':
-        quote=Quote.objects.get(data=id)
-        quote.delete()
-        print("quote_deleted")
+        elif properties.content_type== 'quote_deleted':
+            quote=Quote.objects.get(data=id)
+            quote.delete()
+            print("quote_deleted")
 
 
 channel.basic_consume(queue='likes',on_message_callback=callback,auto_ack=True)
